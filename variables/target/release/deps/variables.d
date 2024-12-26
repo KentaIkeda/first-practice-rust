3b891b28d8387655df3bd20b4b1443f3cee3834d
Kenta Ikeda <77259023+Krudaovo@users.noreply.github.com>
2024-12-27 01:33:34 +0900

    Add generated files and update build artifacts in target directory

---
+++ variables/target/release/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\airon\Desktop\rust\first-practice-rust\variables\target\release\deps\variables.exe: src/main.rs
+
+C:\Users\airon\Desktop\rust\first-practice-rust\variables\target\release\deps\variables.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/release/variables.d
@@ -0,0 +1,1 @@
+C:\Users\airon\Desktop\rust\first-practice-rust\variables\target\release\variables.exe: C:\Users\airon\Desktop\rust\first-practice-rust\variables\src\main.rs
